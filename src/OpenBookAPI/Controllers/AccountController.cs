31f933192f3a01e394514472eab1948c5c744902
John Bradshaw <john.bradshaw@senior.co.uk>
2015-10-27 15:15:27 +0000

    moved swagger to myget, tried to get auth0 working

--- src/OpenBookAPI/Controllers/AccountController.cs
+++ src/OpenBookAPI/Controllers/AccountController.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Cors.Core;
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace OpenBookAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize, Route("api/[controller]"), EnableCors("OpenBookAPI")]
     public class AccountController : Controller
     {
         
         [HttpGet]
-        [Authorize]
+        
         public int Get()
         {
             return 200;
